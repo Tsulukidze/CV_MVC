f65c3e6d405f748a8affff13c7ed2dd1e3f6b9c3
Tsulukidze <temur.tsulukidze@gmail.com>
2022-05-02 16:48:01 +0300

    First Stage of My Cv Project created UserLayout created deffault controller make database connection using Entity-Framework (DB First) added bootstrap files

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CV_MVC.Models.Entity;
+
+namespace CV_MVC.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCVEntities db = new DbCVEntities();
+        public ActionResult Index()
+        {
+            var values = db.TBLABOUT.ToList();
+            return View(values);
+        }
+    }
+}
