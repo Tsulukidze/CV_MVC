795474c68a3a574ce7b4935d1f588f9145d66e4d
Tsulukidze <temur.tsulukidze@gmail.com>
2022-05-04 20:33:55 +0300

    Repository Design Pattern Created Generic Repository class where we can do CRUD proccesses every Table from DBCV has relationship with GenericRepository.cs You can see new files under Repositories folder for example : AboutRepository.cs

---
+++ Repositories/AboutRepository.cs
@@ -0,0 +1,12 @@
+﻿using CV_MVC.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CV_MVC.Repositories
+{
+    public class AboutRepository : GenericRepository<TBLABOUT>
+    {
+    }
+}
---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using CV_MVC.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CV_MVC.Repositories
+{
+    public class GenericRepository<T> where T: class,new()
+    {
+        DbCVEntities db = new DbCVEntities();
+
+        public List<T> List()
+        {
+            return db.Set<T>().ToList();
+        }
+
+        public void TAdd(T obj)
+        {
+            db.Set<T>().Add(obj);
+            db.SaveChanges();
+        }
+
+
+        public void TDelete(T obj)
+        {
+            db.Set<T>().Remove(obj);
+            db.SaveChanges();
+        }
+
+
+        public T TGet(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+
+        public void TUpdate(T obj)
+        {
+            db.SaveChanges();
+        }
+    }
+}
