01e15b0fd902c7b0bc12c9b3862032b16e85fa38
Tsulukidze <temur.tsulukidze@gmail.com>
2022-05-04 21:59:12 +0300

    Added Experience Controller this controller controlls CRUD operations of experiences in AdminLayout created AddExperience method which has post and get actions created and modified AddExperiene.cshtml

---
+++ Controllers/ExperienceController.cs
@@ -0,0 +1,34 @@
+﻿using CV_MVC.Models.Entity;
+using CV_MVC.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV_MVC.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        // GET: Experience
+        ExperienceRepository repo = new ExperienceRepository();
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddExperience()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddExperience(TBLEXPERIENCE obj)
+        {
+            repo.TAdd(obj);
+            return RedirectToAction("Index");
+        }
+    }
+}
